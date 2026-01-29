dbcbd965f23aa3105d981fd30296751f63bd5930
JS <astrojord@gmail.com>
2026-01-28 23:27:13 -0500

    add mason LSPs, COQ, and treesitter to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -29,33 +29,99 @@ require("lazy").setup({
   spec = {
     -- add your plugins here
     {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
+      'nvim-lualine/lualine.nvim',
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
     },
     {
-        'goolord/alpha-nvim',
-        config = function ()
-            require'alpha'.setup(require'alpha.themes.dashboard'.config)
-        end,
+      'goolord/alpha-nvim',   --greeter
+      config = function()
+        require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
+      end,
     },
     { 'Mofiqul/dracula.nvim' },
     {
-        "nvim-tree/nvim-tree.lua",
-        version = "*",
-        lazy = false,
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("nvim-tree").setup {}
-        end,
+      "nvim-tree/nvim-tree.lua",
+      version = "*",
+      lazy = false,
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+      config = function()
+        require("nvim-tree").setup {}
+      end,
     },
     {
-        'nvim-telescope/telescope.nvim', version = '*',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            -- optional but recommended
-            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      'nvim-telescope/telescope.nvim',
+      version = '*',                                    -- fuzzy finder/grepper
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- optional but recommended
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      }
+    },
+    {
+      "mason-org/mason.nvim", -- LSP manager
+      opts = {
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
         }
-    }
+      }
+    },
+    {
+      "neovim/nvim-lspconfig",   -- REQUIRED: for native Neovim LSP integration
+      lazy = false,              -- REQUIRED: tell lazy.nvim to start this plugin at startup
+      dependencies = {
+        -- main one
+        { "ms-jpq/coq_nvim",       branch = "coq" },
+
+        -- 9000+ Snippets
+        { "ms-jpq/coq.artifacts",  branch = "artifacts" },
+
+        -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
+        -- Need to **configure separately**
+        { 'ms-jpq/coq.thirdparty', branch = "3p" }
+      },
+      init = function()
+        vim.g.coq_settings = {
+          auto_start = true, -- if you want to start COQ at startup
+          -- Your COQ settings here
+        }
+      end,
+      config = function()
+        -- Your LSP settings here
+      end,
+    },
+    {
+      'nvim-treesitter/nvim-treesitter',
+      lazy = false,
+      build = ':TSUpdate',
+      config = function()
+        require 'nvim-treesitter'.setup {
+          -- A list of parser names, or "all" (the five listed parsers should always be installed)
+          ensure_installed = { "lua", "vim", "markdown", "markdown_inline", "bash", "git_rebase", "gitcommit", "python", "rust", "yaml", "terraform", "sql", "html", "js" },
+          highlight = {
+            enable = true,
+
+            -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+            disable = function(lang, buf)
+              local max_filesize = 100 * 1024 -- 100 KB
+              local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+              if ok and stats and stats.size > max_filesize then
+                return true
+              end
+            end,
+
+            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+            -- Using this option may slow down your editor, and you may see some duplicate highlights.
+            -- Instead of true it can also be a list of languages
+            additional_vim_regex_highlighting = false,
+          },
+        }
+      end
+    },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
@@ -69,8 +135,8 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'dracula',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
     disabled_filetypes = {
       statusline = {},
       winbar = {},
@@ -99,18 +165,19 @@ require('lualine').setup {
     }
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = { 'filename' },
+
+    lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {}
   },
@@ -139,7 +206,12 @@ require('lualine').setup {
 
 -- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
--- Set theme
+-- Various vim settings
+vim.opt.number = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.wrap = false
+vim.opt.cursorline = true
 vim.cmd.colorscheme 'dracula'
 
 -- Set telescope bindings
